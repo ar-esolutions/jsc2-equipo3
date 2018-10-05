e23fa21690b1fa0646084e53186af67507961f2d
Ismael <ismael.juarez@outlook.com>
2018-10-05 19:58:00 -0300

    Creada clase hotel

---
+++ src/main/java/com/esolutions/trainings/jsc2/model/Room.java
@@ -0,0 +1,57 @@
+package com.esolutions.trainings.jsc2.model;
+
+public class Room {
+    private int id;
+    private int floor;
+    private int nro;
+    private String type;
+
+    public Room(int i, int f, int n, String t){
+
+        id = i;
+        floor = f;
+        nro = n;
+        type = t; }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public int getNro() {
+        return nro;
+    }
+
+    public void setNro(int nro) {
+        this.nro = nro;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", floor=" + floor +
+                ", nro=" + nro +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
