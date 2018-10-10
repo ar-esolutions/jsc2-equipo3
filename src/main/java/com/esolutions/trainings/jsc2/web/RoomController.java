d091ca6738e18c6e9c8d276ff0f4d034297db989
Ismael <ismael.juarez@outlook.com>
2018-10-10 20:14:38 -0300

    Creada clase listsD

--- src/main/java/com/esolutions/trainings/jsc2/web/RoomController.java
+++ src/main/java/com/esolutions/trainings/jsc2/web/RoomController.java
@@ -1,16 +1,14 @@
 package com.esolutions.trainings.jsc2.web;
 
 import com.esolutions.trainings.jsc2.model.Ejercicio1.Hotel;
-import com.esolutions.trainings.jsc2.model.Ejercicio2.Lista;
 import com.esolutions.trainings.jsc2.model.Ejercicio2.Reservation;
 import com.esolutions.trainings.jsc2.model.Ejercicio2.Room;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-import java.util.GregorianCalendar;
-import java.util.Calendar;
 import java.util.Date;
 
 
@@ -23,18 +21,18 @@ public class RoomController {
         return new GuestResponse(hotel.getGuest(floor,room));
     }
 
-    @GetMapping(value = "/floors/{floor}/rooms/{room}/book")
-    public String getReq2(@PathVariable int floor, @PathVariable int room) {
-        Lista lista = new Lista();
+    @PostMapping(value = "/floors/{floor}/rooms/{room}/book")
+    public String getReq2(@PathVariable int floor, @PathVariable int room,@PathVariable Date check_in, @PathVariable Date check_out) {
+
         String res = "";
-        Calendar in = new GregorianCalendar();
-        in.set(2018, 9, 3); //ingresando mes 10, devuelve Noviembre.
+        //Calendar in = new GregorianCalendar();
+        //in.set(2018, 9, 3); //ingresando mes 10, devuelve Noviembre.
 
-        Calendar out = new GregorianCalendar();
-        out.set(2018, 9, 19);//ingresando mes 10, devuelve Noviembre.
+        //Calendar out = new GregorianCalendar();
+        //out.set(2018, 9, 19);//ingresando mes 10, devuelve Noviembre.
 
-        Date i = in.getTime();
-        Date o = out.getTime();
+        //Date i = in.getTime();
+        //Date o = out.getTime();
 
         long id = 0; // aqui consultar a bd
         int f = floor;
@@ -42,9 +40,9 @@ public class RoomController {
         String t = ""; // aqui consultar a bd
 
         Room ro = new Room(id, f, n, t);
-        Reservation r = new Reservation(id, i, o, ro);
+        Reservation r = new Reservation(id, check_in, check_out, ro);
         res= ""+r.calcularPrecio(ro);
-        lista.agregarAlInicio(r);
+        //lista.agregarAlInicio(r);
         //aqui falta insert de reserva a BD
 
         return res;
