478ae910e91db82fbef87daad2aa3d82dff63ac9
Paolo Emilio Mattio <pemattio@gmail.com>
2018-10-10 20:20:05 -0300

    agregado service a controller de room

--- src/main/java/com/esolutions/trainings/jsc2/web/RoomController.java
+++ src/main/java/com/esolutions/trainings/jsc2/web/RoomController.java
@@ -1,9 +1,12 @@
 package com.esolutions.trainings.jsc2.web;
 
+import com.esolutions.trainings.jsc2.logic.GuestRepeatedService;
+import com.esolutions.trainings.jsc2.logic.RoomService;
 import com.esolutions.trainings.jsc2.model.Ejercicio1.Hotel;
 import com.esolutions.trainings.jsc2.model.Ejercicio2.Lista;
 import com.esolutions.trainings.jsc2.model.Ejercicio2.Reservation;
 import com.esolutions.trainings.jsc2.model.Ejercicio2.Room;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +20,12 @@ import java.util.Date;
 @RestController
 public class RoomController {
     Hotel hotel = new Hotel(50000);
+    private final RoomService service;
+
+    @Autowired
+    public RoomController(RoomService service) {
+        this.service = service;
+    }
 
     @GetMapping(value = "/floors/{floor}/rooms/{room}")
     public GuestResponse getGuestNumber(@PathVariable int floor, @PathVariable int room){
