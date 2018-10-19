44db08b36871e8f24d1079c235c7e631dd031db6
lucianoterziotti <lucianoterziotti@outlook.com>
2018-10-19 18:09:17 -0300

    Merge branch 'master' of /Users/lucianoterziotti/IdeaProjects/jsc2-equipo3 with conflicts.

--- src/main/java/com/esolutions/trainings/jsc2/logic/GuestRepeatedService.java
+++ src/main/java/com/esolutions/trainings/jsc2/logic/GuestRepeatedService.java
@@ -1,28 +1,22 @@
 package com.esolutions.trainings.jsc2.logic;
+import com.esolutions.trainings.jsc2.model.ejercicio2.Guest;
 
-import com.esolutions.trainings.jsc2.model.ejercicio5.Guest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
-
 @Service
 public class GuestRepeatedService {
-	private final JpaRepository<Guest, Long> repository;
+		private final JpaRepository<Guest, Long> repository;
 
-	@Autowired
-	public GuestRepeatedService(JpaRepository<Guest, Long> repository) {
-		this.repository = repository;
-	}
+        @Autowired
+        public GuestRepeatedService(JpaRepository<Guest, Long> repository) {
+            this.repository = repository;
+        }
 
 	public ArrayList<Guest> alphabeticallySortedRepeatedGuestsByLastName() {
 		final ArrayList<Guest> allGuests = (ArrayList<Guest>) this.repository.findAll();
 		return allGuests;
-    //comentario de prueba
-		//Write your code here!
-
-
 	}
 
 
